1c08a14cc32c996f1b2d44dadaa090eb2097218c
Nathan Edwards <nathan.edwards@chandco.net>
2014-05-08 13:37:29 +0100

    Reup

---
+++ ch-extras.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Plugin Name: Charlton House Extra Assets
+ * Plugin URI: http://blog.chandco.net
+ * Description: Custom JS and CSS for Charlton House.  Should be integrated with the theme after development
+ * Version: 0.2
+ * Author: Nathan Edwards
+ * Author URI: mailto:nathan.edwards@chandco.net
+ * License: GPL2
+ */
+ 
+
+// Register style sheet.
+
+
+/**
+ * Register style sheet.
+ */
+function chload_gazscript() {
+	die();
+	wp_enqueue_style( 'gz_gaz_style', plugins_url('css/ch-extras.css') );
+}
+add_action( 'wp_enqueue_scripts', 'chload_gazscript' );
+//comment
+wp_enqueue_style( 'gz_gaz_style', plugins_url('css/ch-extras.css') );
+?>
